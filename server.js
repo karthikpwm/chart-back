fb5f404dfc1c96e1842caac086acad4e6965a15f
Naveenrf <naveen@rightfunds.com>
2022-05-11 15:34:37 +0530

    ff

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const app = require('./app');
 let listen = app.listen( process.env.PORT || 3000, () => {
     //app.listen( 3000, () => {
-    //console.log("Express server listening on port %d in %s mode", listen.address().port, app.settings.env);
+    console.log("Express server listening on port %d in %s mode", listen.address().port, app.settings.env);
     console.log("Server listen");
 })
 // 3306
